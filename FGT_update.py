2c0bca5051e604e5c30a58fab195899a206cf9bf
Zhaoxin (Josie) Pan <panz@fortinet-us.com>
2024-07-29 09:26:55 -0700

    first version of FGT_update and L1_switch control

---
+++ FGT_update.py
@@ -0,0 +1,332 @@
+from telnet import TelnetConnection
+from logger import get_logger
+from time import time, sleep
+import json
+import re
+import os
+
+
+logger = get_logger("clear")
+
+def wait_until(
+        condition,
+        *args,
+        timeout=30,
+        period=1,
+        expect_true=True,
+        **kwargs
+):
+    mustend = time() + timeout
+    i = 0
+    while time() < mustend:
+        results = condition(*args, **kwargs)
+        # Checks if results is explicitly false.
+        # Then checks that result against expect_true
+        # ex: results = False & expect_true = True;
+        # (F == F) > T; (T == !T) > False.  returns False
+        # This allows for boolean comparisons without falsifying None, '' and 0.
+        if (results is False) is not expect_true:
+            return results
+        sleep(period)
+        i += period
+    return condition(*args, **kwargs)
+
+
+def get_device_info(name, json_file = 'device_info.json'):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        for platform in data['platforms']:
+            if platform['name'] == name:
+                return platform
+
+
+class FortigateConsole(TelnetConnection):
+    def __init__(
+        self,
+        host,
+        port,
+        console_type="cisco",
+        console_password="",
+        username="admin",
+        password="a",
+        timeout=15,
+        multi_blade=True
+    ):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.console_password = console_password
+        self.logger = get_logger(name=f"cli.{host}")
+        self.telnet_con = None
+        self.multi_blade = multi_blade
+        self.last_out = None
+        self.cmd_find_status = -1
+        self.default_timeout = timeout
+        self.fgt_connected = False
+        self.console_type = console_type
+        super().__init__(self.host, self.port, self.default_timeout)
+
+    def login_console_server(self):
+        self.connect(23)
+        self.get_output(exp=[r":\s*$", r"#\s*$"])
+        if "Password:" in self.last_out:
+            self.send_command(
+                self.console_password,
+                r">\s*$",
+                must_find=True
+            )
+        if self.cmd_find_status > -1:
+            logger.info("Login to console server successful")
+
+    def clear_line(self, line_num=None):
+        if self.console_type not in "cisco":
+            logger.info("Not a cisco console type, no need clear line")
+            return
+        if not line_num:
+            line_num = self.port % 1000
+        logger.info(f"Clearing Line {line_num}")
+        self.login_console_server()
+        if self.output_contains(">"):
+            self.send_command("enable", exp=r":\s*$", must_find=True)
+            self.send_command(
+                self.console_password,
+                exp=[r"#\s*$"],
+                must_find=True
+            )
+        self.send_command(f"clear line {line_num}", exp=r"]\s*$")
+        self.send_command("", exp=r"\[OK\]\s*", must_find=True)
+        logger.info(f"Line {line_num} cleared")
+        self.disconnect()
+
+    def login_fortigate(self):
+        logger.info("Login to fortigate from console")
+        self.clear_line()
+        self.connect()
+        retry = 5
+        while retry > 0:
+            try:
+                self.send_command(
+                    "",
+                    exp=["login:", "#"],
+                    must_find=True,
+                    timeout=3
+                )
+                break
+            except LookupError as e:
+                logger.info("retry login...")
+                retry -= 1
+                if retry <= 0:
+                    raise e
+        if self.output_contains("login:"):
+            self.send_command(f"{self.username}", "Password:")
+            if self.output_contains("Password:"):
+                self.send_command(
+                    f"{self.password}", exp=["#", "Login incorrect"]
+                )
+                if self.output_contains("Login incorrect"):
+                    logger.info("Current password is incorrect")
+                    self.set_password_after_reset()
+                if self.output_contains("#"):
+                    logger.info("Login Successful")
+        else:
+            logger.info("already login to fortigate")
+            self.send_command("end", exp="#")
+
+    def set_password_after_reset(self):
+        logger.info(f"Reset password for user: {self.username}")
+        self.send_command("admin", exp="Password:")
+        self.send_command("", exp=["New Password:", "#"])
+        if self.output_contains("New Password:"):
+            self.send_command(
+                self.password, exp="Confirm Password:", must_find=True
+            )
+            self.send_command(self.password)
+            msg = f"Fortigate Password reset to {self.password}"
+        else:
+            msg = "Fortigate Password updated to None, updated user to admin"
+            self.username = "admin"
+            self.password = ""
+        logger.info(msg)
+
+    # pylint: disable=unsupported-membership-test
+    def wait_fortigate_bootup(self, timeout=600):
+        def wait_func():
+            try:
+                out = self.get_output()
+                return "login:" in out
+            except Exception as e:
+                logger.exception(
+                    "Error when check out put for wait_fortigate_bootup",
+                    exc_info=e
+                )
+            return False
+
+        result = wait_until(wait_func, timeout=timeout, period=5)
+        assert result, "Something went wrong when waiting bootup"
+
+    def back_to_root_level(self):
+        times = 10
+        while times > 0:
+            if not self.output_contains(")"):
+                break
+            self.send_command("end", exp="#", display=False)
+
+    def load_image(self, file, tftp_ip, timeout=120):
+        try:
+            #self.back_to_root_level()
+            self.send_command("config global", exp="(global)", must_find=True)
+            self.send_command(
+                f"execute restore image tftp {file} {tftp_ip}",
+                exp=r"n\)",
+            )
+            self.send_command("y", newline=False)
+            """
+            def get_image_func():
+                out = self.get_output()
+                return "Get image from tftp server OK." in out
+
+            result = wait_until(get_image_func, timeout=timeout, period=5)
+"""
+            end_time = time() + timeout
+            print(end_time)
+            while time() < end_time:
+                out = self.get_output()
+                if "(y/n)" in out:
+                    self.send_command("y", newline=False)
+
+            self.wait_fortigate_bootup()
+            self.set_password_after_reset()
+            logger.info("load image done")
+        except Exception as e:
+            logger.exception("Error when loading image", exc_info=e)
+
+    def load_signature(self, command):
+        try:
+            if not self.output_contains("(global)"):
+                self.send_command("config global", exp="(global)", must_find=True)
+            self.send_command(command, exp=r"n\)", must_find=True)
+            self.send_command("y", newline=False, exp="tftp server OK", timeout=20)
+        except Exception as e:
+            logger.exception("Error when update signatrue", exc_info=e)
+
+    def restore_settings(self, file, tftp_ip):
+        self.back_to_root_level()
+        self.send_command("config global", exp="(global)")
+        self.send_command(
+            f"execute restore config tftp {file} {tftp_ip}",
+            exp=r"n\)",
+            must_find=True
+        )
+        self.send_command("y", newline=False)
+        self.wait_fortigate_bootup()
+
+    def get_current_credential(self):
+        return {"username": self.username, "password": self.password}
+
+    def clear_session(self):
+        cmd = [
+            "config global",
+            "diagnose sys session filter policy 1",
+            "diag sys session clear",
+            "diag sys session6 clear",
+            "end"
+        ]
+        self.send_commands(cmd)
+
+    def check_blade_sync(self):
+        if self.multi_blade:
+            cmd = [
+                "config global",
+                "diag sys confsync status",
+                "end"
+            ]
+
+            out = self.send_commands(cmd)
+            if len(out) > 0:
+                in_sync_re = re.compile(r"in_sync=(.*?)$")
+                members_re = re.compile(r"members:(.*?)$")
+                out = out[1].split("\n")
+                is_status = False
+                members = 0
+                in_sync = 0
+                not_in_sync = []
+                for line_number, line in enumerate(out):
+                    if line.startswith("====="):
+                        if out[line_number + 1].startswith("MBD"):
+                            is_status = True
+                            continue
+                        if is_status:
+                            break
+                    if is_status:
+                        if members == 0:
+                            member_match = members_re.search(line)
+                            if member_match:
+                                members = int(member_match.group(1))
+                        in_sync_match = in_sync_re.search(line)
+                        if in_sync_match:
+                            sync_stat = int(in_sync_re.search(line).group(1))
+                            if sync_stat == 1:
+                                in_sync += 1
+                            else:
+                                not_in_sync.append(line)
+
+                if in_sync > 0 and in_sync == members:
+                    logger.info(
+                        f"All Slaves are in sync with master, "
+                        f"members = {members}, in_sync={in_sync}"
+                    )
+                    return True
+
+                err = "\n".join(not_in_sync)
+                logger.info(
+                    f"Slave \n {err}\n not in sync"
+                )
+                return False
+        else:
+            logger.info("Not multi-blade system, no need check sync status")
+            return
+
+
+if __name__ == "__main__":
+    topology_name = os.getenv('TOPOLOGY_NAME')
+    #topology_name = 'FGT6501F'
+    atp_str = os.getenv('ATP')
+    #atp = {"ftp": "10.160.57.62", "os_image": "FGT_6000F-v7-build1677-FORTINET.out", "os_image_info": {"project": "FortiOS", "version": "7", "build": "3376", "file_pattern": "FGT_3501F-.*\\.out", "branch": "main"}, "os_product": "FortiOS", "os_ver": "7.4.4dev", "os_build": "3376", "os_prefix": "FGT_3501F", "os_label": "Interim Build < Target Version >", "ips_image": "", "ips_image_info": {"project": "IPSengine", "version": "7", "build": "0183", "file_pattern": "flen-fos\\d+\\.\\d+-\\d\\.\\d{3}\\.pkg", "branch": "main"}, "ips_ver": "7.0.16dev", "ips_build": "0183", "ips_label": "Interim Build", "config": "test-config-BMRK2", "config_file_id": "", "config_version": "", "config_build": "", "config_checksum": "", "signature": {"apdb": [{"version": "27.789", "file": "apdb_OS7.4.0_27.00789.APDB.pkg"}], "fmwp": [], "iotd": [], "isdb": [{"version": "27.785", "file": "isdb_OS7.4.0_27.00785.ISDB.pkg"}], "nids": [{"version": "27.790", "file": "nids_OS7.4.0_27.00790.NIDS.pkg"}], "otdb": [], "otdp": [], "etdb": [{"version": "92.03702", "file": "vsigupdate-OS7.4.0_92.03702.ETDB.High.pkg"}], "exdb": [{"version": "92.04510", "file": "/exdb/vsigupdate-OS7.0.0_92.04510.EXDB.pkg"}], "mmdb": [{"version": "92.03702", "file": "vsigupdate-OS7.4.0_92.03702.MMDB.pkg"}], "fldb": [{"version": "92.03702", "file": "vsigupdate-OS7.4.0_92.03702.FLDB.pkg"}], "avai": [{"version": "2.16370", "file": "/avai/vsigupdate-OS7.0.0_2.16370.AVAI.pkg"}]}, "signature_path": "signature/7.0"}
+    atp = json.loads(atp_str)
+    device = get_device_info(topology_name)
+    
+    tftp_ip = atp['ftp']
+    if not atp['os_image']:
+        os_file = f"{atp['os_prefix']}-v{atp['os_image_info']['version']}-build{atp['os_build']}-FORTINET.out"
+    else:
+        os_file = atp['os_image']
+    mmdb_file =  f'{atp["signature_path"]}{atp["signature"]["mmdb"][0]["file"]}'
+    fldb_file =  f'{atp["signature_path"]}{atp["signature"]["fldb"][0]["file"]}'
+    etdb_file =  f'{atp["signature_path"]}{atp["signature"]["etdb"][0]["file"]}'   
+    apdb_file =  f'{atp["signature_path"]}{atp["signature"]["apdb"][0]["file"]}'
+    nids_file =  f'{atp["signature_path"]}{atp["signature"]["nids"][0]["file"]}'
+    isdb_file =  f'{atp["signature_path"]}{atp["signature"]["isdb"][0]["file"]}'
+    command_mmdb = f"execute restore av tftp {mmdb_file} {atp['ftp']}"
+    command_fldb = f"execute restore av tftp {fldb_file} {atp['ftp']}"
+    command_etdb = f"execute restore av tftp {etdb_file} {atp['ftp']}"
+    command_apdb = f"execute restore ips tftp {apdb_file} {atp['ftp']}"
+    command_nids = f"execute restore ips tftp {nids_file} {atp['ftp']}"
+    command_isdb = f"execute restore ips tftp {isdb_file} {atp['ftp']}"
+
+    con = FortigateConsole(
+        device['console_ip'], port=device['console_port'], username=device['username'], password=device['password'],
+        multi_blade=False, console_type='esxi')
+    con.clear_line()
+    con.login_fortigate()
+    con.clear_line()
+    con.load_image(os_file, tftp_ip)
+    con.load_signature(command_apdb)
+    con.load_signature(command_nids)
+    con.load_signature(command_isdb)
+    con.load_signature(command_mmdb)
+    con.load_signature(command_fldb)
+    con.load_signature(command_etdb)
+    con.clear_line()
+    con.login_fortigate()
+    con.clear_session()
--- L1_switch.py
+++ L1_switch.py
@@ -1,10 +1,6 @@
 import requests
 import time
 import os
-import re
-import telnetlib
-import logging
-import json
 
 
 class NetScoutSw:
@@ -73,12 +69,20 @@ class NetScoutSw:
         return self.send(method, url)
 
 
+def map_topology(topology_name):
+    map_dict = {
+        "FGT6501F": "6501F-gen2",
+        "FGT7040E-2": "7040E-2",
+        "FGT7KF": "7KF-72"
+        }
+    return map_dict[topology_name]
+
 def activate_topology(topology_name):
     ip = "10.160.70.74"
     user = "BMRKAUTO"
     password = "netscout2"
     sw = NetScoutSw(ip, user, password)
-    response_activate = response_activate = sw.operate_topology(topology_name, "activate")
+    response_activate = response_activate = sw.operate_topology(map_topology(topology_name), "activate")
     assert "Successful" in response_activate["message"]
     
     time.sleep(30)
@@ -90,181 +94,17 @@ def deactivate_topology(topology_name):
     user = "BMRKAUTO"
     password = "netscout2"
     sw = NetScoutSw(ip, user, password)
-    response_activate = response_activate = sw.operate_topology(topology_name, "deactivate")
+    response_activate = response_activate = sw.operate_topology(map_topology(topology_name), "deactivate")
     assert "Successful" in response_activate["message"]
     
     time.sleep(30)
     sw.logout()
 
 
-class TelnetConnection:
-    def __init__(self, host, port=23, timeout=15):
-        self.host = host
-        self.port = port
-        self.telnet_con = None
-        self.last_out = None
-        self.cmd_find_status = -1
-        self.default_timeout = timeout
-        self._last_port = port
-        self.logger = logging.getLogger(__name__)
-
-    def connect(self, port=None):
-        if not port:
-            port = self.port
-        self._last_port = port
-        retry = 10
-        error = None
-        while retry > 0:
-            try:
-                self.telnet_con = telnetlib.Telnet(self.host, port)
-                self.logger.info("Console login successful")
-                return
-            except ConnectionRefusedError as e:
-                self.logger.error("Console connect fail, retrying..")
-                error = e
-                retry -= 1
-                time.sleep(3)
-        if error:
-            raise error
-
-    def login(self, username, password):
-        self.get_output("login: ")
-        self.send_command(username)
-        self.get_output("Password: ")
-        self.send_command(password)
-        self.get_output("# ")  # Adjust this prompt as needed
-
-    def disconnect(self, ext_cmd="exit"):
-        try:
-            if ext_cmd:
-                self.send_command(ext_cmd)
-            if self.telnet_con:
-                self.telnet_con.close()
-        except Exception as e:
-            self.logger.exception("Error when disconnect", exc_info=e)
-        self.telnet_con = None
-
-    def get_output(self, exp=None, timeout=None, must_find=False, display=True, waiting=0):
-        try:
-            if not timeout:
-                timeout = self.default_timeout
-            if isinstance(exp, str):
-                exp = [exp]
-            if not exp:
-                time.sleep(waiting)
-                out = self.telnet_con.read_very_eager()
-            else:
-                exp = list(map(lambda x: x.encode(), exp))
-                findings, _, out = self.telnet_con.expect(exp, timeout)
-                self.cmd_find_status = findings
-                if exp and findings == -1:
-                    msg = f"Can not find {exp}"
-                    if must_find:
-                        raise LookupError(msg)
-                    self.logger.error(msg)
-            self.last_out = out.decode()
-            if self.last_out and display:
-                self.logger.info(self.last_out)
-            return self.last_out
-        except EOFError:
-            self.logger.exception("EOF for get_output")
-
-    def output_contains(self, content):
-        return content in self.last_out
-
-    def output_match(self, regx):
-        return re.match(regx, self.last_out) is not None
-
-    def send_command(self, cmd, exp=None, timeout=None, must_find=False, newline=True, display=True, waiting=0):
-        if display:
-            self.logger.info(f">>> SEND: {cmd}")
-        cmd = cmd.encode("ascii")
-        if newline:
-            cmd += b"\n"
-        if not self.telnet_con:
-            self.connect(self._last_port)
-        self.telnet_con.write(cmd)
-        if not timeout:
-            timeout = self.default_timeout
-        self.get_output(exp, timeout, must_find, display=display, waiting=waiting)
-        return self.last_out
-
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    # 从环境变量获取参数
     topology_name = os.getenv('TOPOLOGY_NAME')
-    atp_str = os.getenv('ATP')
-    #atp = {"ftp": "10.160.57.62", "os_image": "", "os_image_info": {"project": "FortiOS", "version": "7", "build": "3376", "file_pattern": "FGT_3501F-.*\\.out", "branch": "main"}, "os_product": "FortiOS", "os_ver": "7.4.4dev", "os_build": "3376", "os_prefix": "FGT_3501F", "os_label": "Interim Build < Target Version >", "ips_image": "", "ips_image_info": {"project": "IPSengine", "version": "7", "build": "0183", "file_pattern": "flen-fos\\d+\\.\\d+-\\d\\.\\d{3}\\.pkg", "branch": "main"}, "ips_ver": "7.0.16dev", "ips_build": "0183", "ips_label": "Interim Build", "config": "test-config-BMRK2", "config_file_id": "", "config_version": "", "config_build": "", "config_checksum": "", "signature": {"apdb": [{"version": "27.789", "file": "apdb_OS7.4.0_27.00789.APDB.pkg"}], "fmwp": [], "iotd": [], "isdb": [{"version": "27.785", "file": "isdb_OS7.4.0_27.00785.ISDB.pkg"}], "nids": [{"version": "27.790", "file": "nids_OS7.4.0_27.00790.NIDS.pkg"}], "otdb": [], "otdp": [], "etdb": [{"version": "92.03702", "file": "vsigupdate-OS7.4.0_92.03702.ETDB.High.pkg"}], "exdb": [{"version": "92.04510", "file": "/exdb/vsigupdate-OS7.0.0_92.04510.EXDB.pkg"}], "mmdb": [{"version": "92.03702", "file": "vsigupdate-OS7.4.0_92.03702.MMDB.pkg"}], "fldb": [{"version": "92.03702", "file": "vsigupdate-OS7.4.0_92.03702.FLDB.pkg"}], "avai": [{"version": "2.16370", "file": "/avai/vsigupdate-OS7.0.0_2.16370.AVAI.pkg"}]}, "signature_path": "signature/7.0"}
-    atp = json.loads(atp_str)
-    activate_topology(topology_name)
-
-    # 远程设备登录信息
-    ip = '10.160.18.223'
-    username = 'admin'
-    password = 'a'
-    command = 'execute reboot'
-    os_file = f"{atp['os_prefix']}-v{atp['os_image_info']['version']}-build{atp['os_build']}-FORTINET.out"
-    mmdb_file = ', '.join(entry['file'] for entry in atp['signature']['mmdb'])
-    fldb_file = ', '.join(entry['file'] for entry in atp['signature']['fldb'])
-    etdb_file = ', '.join(entry['file'] for entry in atp['signature']['etdb'])
-    apdb_file = ', '.join(entry['file'] for entry in atp['signature']['apdb'])
-    nids_file = ', '.join(entry['file'] for entry in atp['signature']['nids'])
-    isdb_file = ', '.join(entry['file'] for entry in atp['signature']['isdb'])
-    command_os = f"execute restore image tftp {os_file} {atp['ftp']}"
-    command_mmdb = f"execute restore av tftp {mmdb_file} {atp['ftp']}"
-    command_fldb = f"execute restore av tftp {fldb_file} {atp['ftp']}"
-    command_etdb = f"execute restore av tftp {etdb_file} {atp['ftp']}"
-    command_apdb = f"execute restore ips tftp {apdb_file} {atp['ftp']}"
-    command_nids = f"execute restore ips tftp {nids_file} {atp['ftp']}"
-    command_isdb = f"execute restore ips tftp {isdb_file} {atp['ftp']}"
-
-    # 创建 TelnetConnection 对象并执行命令
-    telnet_conn = TelnetConnection(ip)
-    telnet_conn.connect()
-    telnet_conn.login(username, password)
-    time.sleep(5)
-    telnet_conn.send_command('c g')
-    time.sleep(6)
-    telnet_conn.send_command(command_os)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    if telnet_conn.get_output('(y/n)'):
-        telnet_conn.send_command('y')
-    time.sleep(30)
-    telnet_conn.send_command('')
-    time.sleep(600)
-    telnet_conn.disconnect()
-
-    time.sleep(180)
-    telnet_conn = TelnetConnection(ip)
-    telnet_conn.connect()
-    telnet_conn.login(username, password)
-    time.sleep(5)
-    telnet_conn.send_command('c g')
-    time.sleep(3)
-    telnet_conn.send_command(command_mmdb)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    time.sleep(60)
-    telnet_conn.send_command(command_fldb)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    time.sleep(60)
-    telnet_conn.send_command(command_etdb)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    time.sleep(60)
-    telnet_conn.send_command(command_apdb)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    time.sleep(60)
-    telnet_conn.send_command(command_nids)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    time.sleep(60)
-    telnet_conn.send_command(command_isdb)
-    telnet_conn.get_output('(y/n)')
-    telnet_conn.send_command('y')
-    time.sleep(60)
-    telnet_conn.disconnect()
+    L1_status = os.getenv('L1_status')
+    if L1_status == 'activate':
+        activate_topology(topology_name)
+    elif L1_status == 'deactivate':
+        deactivate_topology(topology_name)
