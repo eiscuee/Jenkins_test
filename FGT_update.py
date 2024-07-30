11937dcbeff7f1233948d2ddb7afb0664acbe24e
Zhaoxin (Josie) Pan <panz@fortinet-us.com>
2024-07-30 11:33:47 -0700

    modify signature load function

--- FGT_update.py
+++ FGT_update.py
@@ -288,9 +288,9 @@ class FortigateConsole(TelnetConnection):
 
 
 if __name__ == "__main__":
-    #topology_name = os.getenv('TOPOLOGY_NAME')
-    topology_name = 'FGT7040E-2'
-    """
+    topology_name = os.getenv('TOPOLOGY_NAME')
+    #topology_name = 'FGT7040E-2'
+    
     if len(sys.argv) < 2:
         logger.info("Usage: python FGT_update.py <atp_setting.json>")
         sys.exit(1)
@@ -302,8 +302,8 @@ if __name__ == "__main__":
         logger.info(atp)
     except Exception as e:
         print(f"Failed to load ATP settings: {e}")
-    """
-    atp = {"ftp": "10.160.90.106", "os_image": "/images/FortiOS/v7.00/build1681/FGT_7000E-v7-build1681-FORTINET.out", "os_image_info": {"project": "FortiOS", "version": "7", "build": "1681", "file_pattern": "FGT_7000E-.*\\.out", "branch": "main"}, "os_product": "FortiOS", "os_ver": "7.2.9dev", "os_build": "1681", "os_prefix": "FGT_7000E", "os_label": "Interim Build < Target Version >", "ips_image": "/images/IPSEngine/v7.00/build0342/flen-fos7.2-7.342.pkg", "ips_image_info": {"project": "IPSengine", "version": "7", "build": "0342", "file_pattern": "flen-fos\\d+\\.\\d+-\\d\\.\\d{3,4}\\.pkg", "branch": "main"}, "ips_ver": "7.2.9dev", "ips_build": "0342", "ips_label": "Interim Build", "config": "BMRK-SLBC-7040E-2", "config_file_id": "", "config_version": "", "config_build": "", "config_checksum": "", "signature": {"apdb": [{"version": "28.833", "file": "/apdb/apdb-720-28.833.pkg"}], "fmwp": [{"version": "24.070", "file": "/fmwp/fmwp-720-24.070.pkg"}], "iotd": [{"version": "28.833", "file": "/iotd/iotd-720-28.833.pkg"}], "isdb": [{"version": "28.827", "file": "/isdb/isdb-720-28.827.pkg"}], "nids": [{"version": "28.833", "file": "/nids/nids-720-28.833.pkg"}], "otdb": [], "otdp": [], "avdb": [{"version": "92.06178", "file": "/avdb/vsigupdate-OS7.2.0_92.06178.ETDB.High.pkg"}], "exdb": [{"version": "92.06061", "file": "/exdb/vsigupdate-OS7.2.0_92.06061.EXDB.pkg"}], "mmdb": [{"version": "92.06177", "file": "/mmdb/vsigupdate-OS7.2.0_92.06177.MMDB.pkg"}], "fldb": [{"version": "92.06178", "file": "/fldb/vsigupdate-OS7.2.0_92.06178.FLDB.pkg"}], "avai": [{"version": "2.17356", "file": "/avai/vsigupdate-OS7.2.0_2.17356.AVAI.pkg"}]}, "signature_path": "signature/7.2"}
+    
+    #atp = {"ftp": "10.160.90.106", "os_image": "/images/FortiOS/v7.00/build1681/FGT_7000E-v7-build1681-FORTINET.out", "os_image_info": {"project": "FortiOS", "version": "7", "build": "1681", "file_pattern": "FGT_7000E-.*\\.out", "branch": "main"}, "os_product": "FortiOS", "os_ver": "7.2.9dev", "os_build": "1681", "os_prefix": "FGT_7000E", "os_label": "Interim Build < Target Version >", "ips_image": "/images/IPSEngine/v7.00/build0342/flen-fos7.2-7.342.pkg", "ips_image_info": {"project": "IPSengine", "version": "7", "build": "0342", "file_pattern": "flen-fos\\d+\\.\\d+-\\d\\.\\d{3,4}\\.pkg", "branch": "main"}, "ips_ver": "7.2.9dev", "ips_build": "0342", "ips_label": "Interim Build", "config": "BMRK-SLBC-7040E-2", "config_file_id": "", "config_version": "", "config_build": "", "config_checksum": "", "signature": {"apdb": [{"version": "28.833", "file": "/apdb/apdb-720-28.833.pkg"}], "fmwp": [{"version": "24.070", "file": "/fmwp/fmwp-720-24.070.pkg"}], "iotd": [{"version": "28.833", "file": "/iotd/iotd-720-28.833.pkg"}], "isdb": [{"version": "28.827", "file": "/isdb/isdb-720-28.827.pkg"}], "nids": [{"version": "28.833", "file": "/nids/nids-720-28.833.pkg"}], "otdb": [], "otdp": [], "avdb": [{"version": "92.06178", "file": "/avdb/vsigupdate-OS7.2.0_92.06178.ETDB.High.pkg"}], "exdb": [{"version": "92.06061", "file": "/exdb/vsigupdate-OS7.2.0_92.06061.EXDB.pkg"}], "mmdb": [{"version": "92.06177", "file": "/mmdb/vsigupdate-OS7.2.0_92.06177.MMDB.pkg"}], "fldb": [{"version": "92.06178", "file": "/fldb/vsigupdate-OS7.2.0_92.06178.FLDB.pkg"}], "avai": [{"version": "2.17356", "file": "/avai/vsigupdate-OS7.2.0_2.17356.AVAI.pkg"}]}, "signature_path": "signature/7.2"}
     device = get_device_info(topology_name)
     
     tftp_ip = atp['ftp']
@@ -330,7 +330,7 @@ if __name__ == "__main__":
     con.clear_line()
     con.login_fortigate()
     con.clear_line()
-    #con.load_image(os_file, tftp_ip)
+    con.load_image(os_file, tftp_ip)
     con.send_command("config global")
     con.load_signature(command_apdb)
     con.load_signature(command_nids)
